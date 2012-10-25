9322e2ac0c58bea5cd88f4685060bab97fee81af
Dto <dto80@yahoo.com>
2012-10-25 22:04:25 +0700

    update the user detail to show the local name, local description without setting the local outside the user object, and update the security/users/show.jspx to show the user name (according to the localization)

--- src/main/java/edu/cmucdu/ecommerce/domain/user/UserDetail.java
+++ src/main/java/edu/cmucdu/ecommerce/domain/user/UserDetail.java
@@ -1,16 +1,24 @@
 package edu.cmucdu.ecommerce.domain.user;
 
+import java.util.Locale;
+
 import javax.persistence.CascadeType;
 import javax.persistence.OneToOne;
 import javax.persistence.Transient;
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import edu.cmucdu.ecommerce.domain.user.security.Principal;
 import edu.cmucdu.ecommerce.domain.util.Description;
 import edu.cmucdu.ecommerce.domain.util.LocaleEnum;
+import edu.cmucdu.ecommerce.web.util.WebUtil;
 
 @RooJavaBean
 @RooToString
@@ -31,7 +39,12 @@ public abstract class UserDetail {
 	@OneToOne(cascade=CascadeType.ALL)
 	Description description;
 	
+	
 	public String getLocalName(){
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+
+				.getRequestAttributes()).getRequest();
+		locale = WebUtil.getLocaleEnum(request);
 		if (locale == LocaleEnum.CHINESE){
 			return name.getChineseDesc();
 		}else if(locale == LocaleEnum.THAI ){
@@ -42,6 +55,10 @@ public abstract class UserDetail {
 	}
 	
 	public String getLocalDescription(){
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+
+				.getRequestAttributes()).getRequest();
+		locale = WebUtil.getLocaleEnum(request);
 		if (locale == LocaleEnum.CHINESE){
 			return description.getChineseDesc();
 		}else if(locale == LocaleEnum.THAI ){
