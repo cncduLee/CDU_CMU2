b96e46084b6ca194244c01b92d8c58e762d5d8cc
cncduLee <shouli1990@gmail.com>
2013-01-21 17:43:26 +0800

    +goodsDetail UI

--- src/main/java/edu/cmucdu/ecommerce/web/cart/CartController.java
+++ src/main/java/edu/cmucdu/ecommerce/web/cart/CartController.java
@@ -2,8 +2,10 @@ package edu.cmucdu.ecommerce.web.cart;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.ServiceMode;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import edu.cmucdu.ecommerce.dao.product.SellerProductDao;
 import edu.cmucdu.ecommerce.domain.product.shoppingcart.Cart;
@@ -22,48 +25,52 @@ import edu.cmucdu.ecommerce.domain.user.security.Principal;
 
 
 @Controller
+@SessionAttributes("myCart")
+
 public class CartController {
-    @Autowired
+	@Autowired
 	SellerProductDao sellerProductDao;
+    
 	
-//    @ModelAttribute("cart")
-//	public Cart getCartModel() {
-//    	System.out.println("======1");
-//    	Cart c = new Cart();
+	@ModelAttribute("myCart")
+	public Cart getCart(){
+		System.out.println("---");
+		Cart c = new Cart();
 //		Principal principal = (Principal) SecurityContextHolder.getContext()
 //				.getAuthentication().getPrincipal();
 //		c.setBuyer((Buyer) principal.getUser());
-//		return c;
+		System.out.println("--@@--");
+		return c;
+	}
+	
+//	@RequestMapping(method = RequestMethod.POST, value = "{id}")
+//	public void post(@PathVariable Long id, ModelMap modelMap,
+//			HttpServletRequest request, HttpServletResponse response) {
+//		System.out.println("======get");
 //	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/cart/{id}")
-	public void post(@PathVariable Long id, ModelMap modelMap,
-			HttpServletRequest request, HttpServletResponse response) {
-		
-		System.out.println("======get");
-		
-	}
-
-	@RequestMapping("cartPage")
+	@RequestMapping(value="cartPage")
 	public String index() {
 		System.out.println("======index");
 		return "cart/index";
 	}
 
-	@RequestMapping(value = "/cartAdd/productId={id}&amount={amount}", method = RequestMethod.GET)
-	public String addProduct(@PathVariable("id") long id,
+	@RequestMapping(value = "cartAdd/productId={id}&amount={amount}", method = RequestMethod.GET)
+	public String addProduct(
+			@PathVariable("id") long id,
 			@PathVariable("amount") int amount,
-			@ModelAttribute("cart") Cart cart, Model uiModel,
+			@ModelAttribute("myCart") Cart cart,
+			Model uiModel,
 			HttpServletRequest httpServletRequest) {
-		
-		System.out.println("======t");
-			CartTransaction ct = new CartTransaction();
-			ct.setCart(cart);
-			ct.setAmount(amount);
-			ct.setSellerProduct(sellerProductDao.findOne(id));
-			//wait for show all the product list
-			return "index";
-		
+		CartTransaction ct = new CartTransaction();
+		ct.setCart(cart);
+		ct.setAmount(amount);
+		ct.setSellerProduct(sellerProductDao.findOne(id));
+		//wait for show all the product list
+		return "redirect:../cartPage";
 	}
+	
+	
+
 
 }
