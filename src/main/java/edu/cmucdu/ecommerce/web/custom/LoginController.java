90ff27ade87ebe2fddbcfa0c6af5b6c4f9d23997
cncduLee <shouli1990@gmail.com>
2013-01-24 14:18:30 +0700

    after login success redirect to refrence page

--- src/main/java/edu/cmucdu/ecommerce/web/custom/LoginController.java
+++ src/main/java/edu/cmucdu/ecommerce/web/custom/LoginController.java
@@ -1,5 +1,6 @@
 package edu.cmucdu.ecommerce.web.custom;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.cmucdu.ecommerce.dao.user.BuyerDao;
 import edu.cmucdu.ecommerce.dao.user.SellerDao;
@@ -23,10 +25,16 @@ public class LoginController {
 	@Autowired
 	BuyerDao buyerDao;
 	@Autowired  
-	private HttpSession session;  
+	private HttpSession session;
+	@Autowired  
+	private HttpServletRequest request;  
+	
+	private String refererPage = "/";
 	
 	@RequestMapping("/toLoginPage")
 	public String goToLoginPage() {
+		refererPage = request.getHeader("referer");
+		System.out.println("--"+refererPage);
 		return "login";// redirect login page
 	}
 
@@ -36,11 +44,10 @@ public class LoginController {
 			@RequestParam String typeName,
 			@RequestParam String ccode,
 			ModelMap errorMap) {
-		// get the data from page
+		
 		int type = Integer.parseInt(typeName.trim());
 		UserDetail user = null;
 		
-//		System.out.println(username+"==="+password);
 		String checkCode = (String) session.getAttribute("checkcode");
 		if(ccode != null && ccode.trim().equals(checkCode)){
 			//ccode is right
@@ -62,7 +69,7 @@ public class LoginController {
 					session.setAttribute("myCart", c);
 				}
 				//redirect
-				return "redirect:/";//redirect homepage
+				return "redirect:"+loginSuccessDirect();//redirect homepage
 			}else{
 				//error
 				errorMap.addAttribute("error", "can't find the user!");
@@ -72,9 +79,9 @@ public class LoginController {
 			errorMap.addAttribute("error", "checkCode is wrong!");
 			return "login";
 		}
-		
-		
-		
 	}
 	
+	private String loginSuccessDirect(){
+		return refererPage==null?"/":refererPage.substring(refererPage.lastIndexOf("/"),refererPage.length());
+	}
 }
