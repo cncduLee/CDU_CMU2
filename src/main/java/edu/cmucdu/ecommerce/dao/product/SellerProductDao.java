25d0636f00dd68c684608f55671414e75c21b9bc
Tonkla <sonsiri25989@hotmail.com>
2013-01-24 13:44:59 +0700

    solve conflict

--- src/main/java/edu/cmucdu/ecommerce/dao/product/SellerProductDao.java
+++ src/main/java/edu/cmucdu/ecommerce/dao/product/SellerProductDao.java
@@ -1,9 +1,14 @@
 package edu.cmucdu.ecommerce.dao.product;
 
+import java.util.List;
+
 import edu.cmucdu.ecommerce.domain.product.SellerProduct;
 import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
 
 @RooJpaRepository(domainType = SellerProduct.class)
 public interface SellerProductDao {
-	public SellerProduct getSellerProductByProductId(Long id);
+	public List<SellerProduct> getSellerProductByProductId(Long id);
+	public List<SellerProduct> getSellerProdectBySellerId(Long id);
+	public SellerProduct getSellerProductByProductIdAndSellerId(Long pid, Long sid);
+	
 }
