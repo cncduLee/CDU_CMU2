ce009287397e07f3f9bbedfed3ef2a739f46f79f
Tonkla <sonsiri25989@hotmail.com>
2013-01-25 14:40:30 +0700

    remove TestDao, LogisticDataOnDemand and LogisticIntegration for the better roo generation

--- src/test/java/edu/cmucdu/ecommerce/domain/logistic/LogisticDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package edu.cmucdu.ecommerce.domain.logistic;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Logistic.class)
-public class LogisticDataOnDemand {
-}
