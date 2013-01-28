218e38b96d3a905c53377a0cce764c6012f1c9ae
cncduLee <shouli1990@gmail.com>
2013-01-28 15:15:19 +0700

    close hibernate auto put data into the database

--- src/main/java/edu/cmucdu/ecommerce/domain/loader/Loader.java
+++ src/main/java/edu/cmucdu/ecommerce/domain/loader/Loader.java
@@ -42,7 +42,7 @@ import edu.cmucdu.ecommerce.service.logistic.LogisticService;
  * Preload test data for in-mem db,<br>
  * <a href="http://forum.springsource.org/showthread.php?84312-How-to-store-sample-data/page2">acknowledgement: this technique</a>
  */
-@Component
+//@Component
 public class Loader implements ApplicationListener<ContextRefreshedEvent>{
 
 	@Autowired
